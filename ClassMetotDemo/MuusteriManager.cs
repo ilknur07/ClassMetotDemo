a0a7e1f0f5e27300e47f69e765ee16efbfe11de9
ilknur <ilknur99@live.de>
2021-01-18 14:40:19 +0100

    Add project files.

---
+++ ClassMetotDemo/MuusteriManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Tebrikler, "+ musteri.Yasi + " yasinda olan Müsteri: " + musteri.Adi + " " + musteri.SoyAdi + "," + " ID-Numarasi: " + musteri.Id + " ile Sisteme eklenmistir!");
+        }
+
+        public void Listele(Musteri musteri)
+        {
+            Console.WriteLine("Musteri : " + musteri.Adi + " " + musteri.SoyAdi + "," + " ID-Numarasi: " + musteri.Id  + " ile listelenmistir!");
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Yasi + " yasinda olan Müsteri: " + musteri.Adi + " " + musteri.SoyAdi + "," + " ID-Numarasi: " + musteri.Id  + " ile Sistemden silinmistir!");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 0123456789;
+            musteri1.Adi = "Hasibe";
+            musteri1.SoyAdi = "Senel";
+            musteri1.Yasi = 20;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 9876543210;
+            musteri2.Adi = "Kerem";
+            musteri2.SoyAdi = "Aksoy";
+            musteri2.Yasi = 22;
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+            musteriManager.Listele(musteri1);
+            musteriManager.Sil(musteri2);
+
+
+
+
+
+
+
+
+        }
+    }
+}
